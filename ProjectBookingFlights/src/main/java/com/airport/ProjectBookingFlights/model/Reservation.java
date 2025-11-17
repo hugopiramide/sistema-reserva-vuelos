82da67d476c822b8ed6ad74e89206aa847c29207
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-17 15:57:32 +0100

    Add Reservation

---
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/model/Reservation.java
@@ -0,0 +1,103 @@
+package com.airport.ProjectBookingFlights.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.OverridesAttribute;
+
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long reservation_id;
+ 
+    @Embedded
+    @AttributeOverride(name = "email", column = @Column(name = "user_email", length = 120, nullable = false))
+    private UserEmail userEmail;
+
+    /* Relation @ManyToOne */
+    private Flight flight;
+
+    @Embedded
+    @AttributeOverride(name = "total", column = @Column(name = "passengers", length = 4))
+    private Passengers passengers;
+
+    @Column(name = "booking_date", nullable = false)
+    private LocalDateTime bookingDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    public Reservation() {
+    }
+
+    public Reservation(UserEmail userEmail, Flight flight, Passengers passengers, LocalDateTime bookingDate,
+            Status status) {
+        this.userEmail = userEmail;
+        this.flight = flight;
+        this.passengers = passengers;
+        this.bookingDate = bookingDate;
+        this.status = status;
+    }
+
+    public Long getReservation_id() {
+        return reservation_id;
+    }
+
+    public void setReservation_id(Long reservation_id) {
+        this.reservation_id = reservation_id;
+    }
+
+    public UserEmail getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(UserEmail userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Passengers getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(Passengers passengers) {
+        this.passengers = passengers;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    
+}
