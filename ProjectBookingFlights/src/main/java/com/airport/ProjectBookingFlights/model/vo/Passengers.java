fc5549626570faab8d7ac7fc06442175fb4b8629
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-17 16:04:24 +0100

    Add Value Object passengers

---
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/model/vo/Passengers.java
@@ -0,0 +1,51 @@
+package com.airport.ProjectBookingFlights.model.vo;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public final class Passengers{
+    
+    private final Integer total;
+
+    public Passengers(){
+        this.total = null;
+    }
+
+    public Passengers(Integer total){
+        this.total = total;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(total);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Passengers other = (Passengers) obj;
+        if (total == null) {
+            if (other.total != null)
+                return false;
+        } else if (!total.equals(other.total))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Passengers [total=" + total + "]";
+    }
+
+}
