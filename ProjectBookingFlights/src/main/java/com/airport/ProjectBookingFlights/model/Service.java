98c9bd63ebebcf427c47b750ae8006b8e230c856
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-17 16:45:10 +0100

    Add entity service

---
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/model/Service.java
@@ -0,0 +1,62 @@
+package com.airport.ProjectBookingFlights.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "service")
+public class Service {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long service_id;
+    
+    @Column(name = "name", length = 100)
+    private String name;
+    
+    @Column(name = "description", length = 255)
+    private String description;
+
+    public Service() {
+        
+    }
+
+    public Service(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(Long service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Service [service_id=" + service_id + ", name=" + name + ", description=" + description + "]";
+    }
+    
+}
