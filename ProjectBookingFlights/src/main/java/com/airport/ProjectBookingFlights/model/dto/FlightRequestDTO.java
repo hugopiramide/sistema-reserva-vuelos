c79139a125065566d9722861effcadd5f350259f
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-18 16:05:31 +0100

    Add Flight DTOs

---
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/model/dto/FlightRequestDTO.java
@@ -0,0 +1,53 @@
+package com.airport.ProjectBookingFlights.model.dto;
+
+import java.time.LocalDateTime;
+
+public class FlightRequestDTO {
+    
+    private Long originAirportId;
+    private Long destinationAirportId;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private Integer capacity;
+
+    public FlightRequestDTO(){
+    }
+
+    public Long getOriginAirportId() {
+        return originAirportId;
+    }
+    public void setOriginAirportId(Long originAirportId) {
+        this.originAirportId = originAirportId;
+    }
+    public Long getDestinationAirportId() {
+        return destinationAirportId;
+    }
+    public void setDestinationAirportId(Long destinationAirportId) {
+        this.destinationAirportId = destinationAirportId;
+    }
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+    public Integer getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightRequestDTO [originAirportId=" + originAirportId + ", destinationAirportId=" + destinationAirportId
+                + ", departureTime=" + departureTime + ", arrivalTime=" + arrivalTime + ", capacity=" + capacity + "]";
+    }
+
+}
---
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/model/dto/FlightResponseDTO.java
@@ -0,0 +1,72 @@
+package com.airport.ProjectBookingFlights.model.dto;
+
+import java.time.LocalDateTime;
+
+public class FlightResponseDTO {
+    
+    private String originAirport;
+    private String destinationAirport;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private Integer capacity;
+    private Integer availableSeats;
+
+    public FlightResponseDTO(){  
+    }
+
+    public String getOriginAirport() {
+        return originAirport;
+    }
+
+    public void setOriginAirport(String originAirport) {
+        this.originAirport = originAirport;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(Integer availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightResponseDTO [originAirport=" + originAirport + ", destinationAirport=" + destinationAirport
+                + ", departureTime=" + departureTime + ", arrivalTime=" + arrivalTime + ", capacity=" + capacity
+                + ", availableSeats=" + availableSeats + "]";
+    }
+
+}
