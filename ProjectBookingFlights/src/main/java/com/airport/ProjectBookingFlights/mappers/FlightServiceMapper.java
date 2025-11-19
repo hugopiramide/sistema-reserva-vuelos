4c7ad46690c52922bf794ef4b17611f9969ee9ab
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-19 17:33:28 +0100

    Add FlightService Mapper

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/FlightServiceMapper.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/FlightServiceMapper.java
@@ -1,9 +1,22 @@
 package com.airport.ProjectBookingFlights.mappers;
 
-import java.security.Provider.Service;
-
+import com.airport.ProjectBookingFlights.model.FlightService;
 import com.airport.ProjectBookingFlights.model.dto.request.FlightServiceRequestDTO;
+import com.airport.ProjectBookingFlights.model.dto.response.FlightServiceResponseDTO;
 
 public class FlightServiceMapper {
     
+    public static FlightService toEntity(FlightServiceRequestDTO flightServiceRequestDTO){
+        FlightService flightService = new FlightService();
+        flightService.setName(flightServiceRequestDTO.getName());
+        flightService.setDescription(flightServiceRequestDTO.getDescription());
+        return flightService;
+    }
+    
+    public static FlightServiceResponseDTO toResponse(FlightService flightService){
+        FlightServiceResponseDTO dto = new FlightServiceResponseDTO();
+        dto.setName(flightService.getName());
+        dto.setDescription(flightService.getDescription());
+        return dto;
+    }
 }
