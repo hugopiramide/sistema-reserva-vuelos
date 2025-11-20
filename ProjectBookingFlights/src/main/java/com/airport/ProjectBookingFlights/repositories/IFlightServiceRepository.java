0a131ae2d9dd7f60f9a8b068106b27177e921a0b
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-20 16:09:48 +0100

    Fix Entity Service to FlightService

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IFlightServiceRepository.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IFlightServiceRepository.java
@@ -1,11 +1,11 @@
 package com.airport.ProjectBookingFlights.repositories;
 
-import java.security.Provider.Service;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.airport.ProjectBookingFlights.model.FlightService;
+
 @Repository
-public interface IFlightServiceRepository extends JpaRepository<Service, Long>{
-    
+public interface IFlightServiceRepository extends JpaRepository<FlightService, Long> {
+
 }
