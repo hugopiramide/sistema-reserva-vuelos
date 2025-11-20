bef841a69bbf04e9b9952787e54ddaf79f4d80a3
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-20 16:49:00 +0100

    Fix return toResponseDTO()

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/AirportMapper.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/AirportMapper.java
@@ -2,6 +2,7 @@ package com.airport.ProjectBookingFlights.mappers;
 
 import com.airport.ProjectBookingFlights.model.Airport;
 import com.airport.ProjectBookingFlights.model.dto.request.AirportRequestDTO;
+import com.airport.ProjectBookingFlights.model.dto.response.AirportResponseDTO;
 import com.airport.ProjectBookingFlights.model.vo.City;
 
 public class AirportMapper {
@@ -13,8 +14,8 @@ public class AirportMapper {
         return airport;
     }
     
-    public static AirportRequestDTO toResponseDTO(Airport airport){
-        AirportRequestDTO dto = new AirportRequestDTO();
+    public static AirportResponseDTO toResponseDTO(Airport airport){
+        AirportResponseDTO dto = new AirportResponseDTO();
         dto.setName(airport.getName());
         dto.setCountry(airport.getCity().getCountry());
         dto.setLocation(airport.getCity().getLocation());
