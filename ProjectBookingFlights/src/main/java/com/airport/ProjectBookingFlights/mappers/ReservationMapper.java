565ab6fc5b29d4d901cafdf2f2c71c7bf7fad8f1
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-21 10:30:52 +0100

    Add missing Attrubute Status

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/ReservationMapper.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/mappers/ReservationMapper.java
@@ -9,33 +9,34 @@ import com.airport.ProjectBookingFlights.model.dto.response.FlightResponseDTO;
 import com.airport.ProjectBookingFlights.model.dto.response.ReservationResponseDTO;
 
 public class ReservationMapper {
-    
-    public static Reservation toEntity(Flight flight, Client client, ReservationRequestDTO reservationRequestDTO){
-        Reservation reservation = new Reservation(flight,client);
+
+    public static Reservation toEntity(Flight flight, Client client, ReservationRequestDTO reservationRequestDTO) {
+        Reservation reservation = new Reservation(flight, client);
         return reservation;
     }
-    
-    public static ReservationResponseDTO toResponseDTO(Reservation reservation){
+
+    public static ReservationResponseDTO toResponseDTO(Reservation reservation) {
         ReservationResponseDTO response = new ReservationResponseDTO();
         response.setBookingDate(reservation.getBookingDate());
+        response.setStatus(reservation.getStatus().name());
         response.setClient(new ClientResponseDTO(
-            reservation.getClient().getName(),
-            reservation.getClient().getFirstName(),
-            response.getClient().getEmail()
-        ));
+                reservation.getClient().getName(),
+                reservation.getClient().getFirstName(),
+                response.getClient().getEmail()));
         response.setFlight(new FlightResponseDTO(
-            reservation.getFlight().getOriginAirport().getName(),
-            reservation.getFlight().getDestinationAirport().getName(),
-            reservation.getFlight().getDepartureTime(),
-            reservation.getFlight().getArrivalTime(),
-            reservation.getFlight().getPassengers().getTotal(),
-            reservation.getFlight().getAvailableSeats()
-        ));
+                reservation.getFlight().getOriginAirport().getName(),
+                reservation.getFlight().getDestinationAirport().getName(),
+                reservation.getFlight().getDepartureTime(),
+                reservation.getFlight().getArrivalTime(),
+                reservation.getFlight().getPassengers().getTotal(),
+                reservation.getFlight().getAvailableSeats()));
         return response;
     }
 
-    public static void updateEntityFromDTO(Reservation existing, Flight flight, Client client, ReservationRequestDTO dto) {
-        if (existing == null || dto == null) return;
+    public static void updateEntityFromDTO(Reservation existing, Flight flight, Client client,
+            ReservationRequestDTO dto) {
+        if (existing == null || dto == null)
+            return;
 
         if (flight != null) {
             existing.setFlight(flight);
@@ -44,5 +45,5 @@ public class ReservationMapper {
             existing.setClient(client);
         }
     }
-    
+
 }
