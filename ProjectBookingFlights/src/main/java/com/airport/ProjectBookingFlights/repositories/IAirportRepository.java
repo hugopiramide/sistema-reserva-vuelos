adf47f46fc4d52f7fe76438822b091fade04ae55
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-22 17:53:16 +0100

    Add custom Query to Airport

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IAirportRepository.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IAirportRepository.java
@@ -1,6 +1,7 @@
 package com.airport.ProjectBookingFlights.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.airport.ProjectBookingFlights.model.Airport;
@@ -8,4 +9,7 @@ import com.airport.ProjectBookingFlights.model.Airport;
 @Repository
 public interface IAirportRepository extends JpaRepository<Airport, Long>{
     
+    // Total vuelos del aeropuerto
+    @Query("SELECT COUNT(f) FROM Flight f WHERE f.origin.id = :airportId OR f.destination.id = :airportId")
+    Long countFlightsByAirportId(Long airportId);
 }
