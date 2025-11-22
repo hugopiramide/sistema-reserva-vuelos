2622360d164a2ae94bef785d6c646bfe6c293ff2
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-22 18:05:43 +0100

    Add custom Query to FlightRepo

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IFlightRepository.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IFlightRepository.java
@@ -1,11 +1,18 @@
 package com.airport.ProjectBookingFlights.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.airport.ProjectBookingFlights.model.Flight;
 
 @Repository
 public interface IFlightRepository extends JpaRepository<Flight, Long> {
+    
+    // Vuelos en los que ha participado un cliente específico
+    @Query("SELECT f FROM Flight f INNER JOIN FETCH f.reservations r WHERE r.client.id = :clientId")
+    Set<Flight> findFlightsByClientId(Long clientId);
 
 }
