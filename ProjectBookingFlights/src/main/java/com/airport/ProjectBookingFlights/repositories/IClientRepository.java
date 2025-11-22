7bcdc0a4a89835271a56cbdcfcd5ab22e5238595
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-22 18:01:59 +0100

    Add custom Query to ClientRepo

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IClientRepository.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IClientRepository.java
@@ -1,6 +1,9 @@
 package com.airport.ProjectBookingFlights.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.airport.ProjectBookingFlights.model.Client;
@@ -8,4 +11,8 @@ import com.airport.ProjectBookingFlights.model.Client;
 @Repository
 public interface IClientRepository extends JpaRepository<Client, Long>{
     
+    // Clientes que han realizado más de N reservas
+    @Query("SELECT c FROM Client c WHERE SIZE(c.reservations) > :minReservations")
+    Set<Client> findClientsWithMoreThanNReservations(int minReservations);
+
 }
