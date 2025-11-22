64273aefd52d6fd4342521f8ca4dbb7f8a956a05
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-22 17:48:31 +0100

    Add custom Query to reservations

--- ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IReservationRepository.java
+++ ProjectBookingFlights/src/main/java/com/airport/ProjectBookingFlights/repositories/IReservationRepository.java
@@ -1,11 +1,18 @@
 package com.airport.ProjectBookingFlights.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.airport.ProjectBookingFlights.model.Reservation;
 
 @Repository
 public interface IReservationRepository extends JpaRepository<Reservation, Long>{
+
+    // Buscar las reservas asociadas a un aeropuerto específico (sin importar si es origen o destino)
+    @Query("SELECT r FROM Reservation r WHERE r.flight.origin.id = :airportId OR r.flight.destination.id = :airportId")
+    Set<Reservation> findByAirportId(Long airportId);
     
 }
